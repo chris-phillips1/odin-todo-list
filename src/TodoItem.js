f20f295e53a9a89903f663c06b2d274d7a5b3086
Chris Phillips <cpphillips13@gmail.com>
2024-10-17 21:19:22 -0400

    Add todo item class

---
+++ src/TodoItem.js
@@ -0,0 +1,9 @@
+export default class TodoItem {
+    constructor(title, description, dueDate, actionDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.actionDate = actionDate;
+        this.priority = priority;
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import TodoItem from './TodoItem.js';
+
+let todo = new TodoItem('Test', 'Test description', '10/20/24', '10/18/24', 'high');
+todo.toString();
